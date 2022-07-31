ba6e16f6e2d5ba0f4251f2413b00644f52c5a24d
bhimrazy <bhimrajyadav977@gmail.com>
2022-07-31 18:27:51 +0545

    Added pytorch directory with initails required files

---
+++ src/pytorch/config.py
@@ -0,0 +1,5 @@
+"""
+This configuration file will store important variables and parameters used across our driver scripts. 
+Instead of re-defining them in every script, we’ll simply define them once here (and thereby make our 
+code cleaner and easier to read).
+"""
