e7d3a4cb73e4faa5740a42d0d85a62798874a52d
Bhimraj Yadav <bhimrajyadav977@gmail.com>
2023-02-11 08:40:28 +0000

    Added backend and frontend

---
+++ backend/src/config.py
@@ -0,0 +1,15 @@
+from typing import Any
+from pydantic import BaseSettings, PostgresDsn
+
+
+class Settings(BaseSettings):
+    """ Application configuration."""
+    DATABASE_URL: PostgresDsn
+    # DATABASE_URL: str
+
+    class Config:
+        """Pydantic config."""
+        env_file = ".env"
+
+
+settings = Settings()
---
+++ backend/src/patients/schemas.py
@@ -0,0 +1,80 @@
+import uuid
+from .models import Patient
+from datetime import datetime
+from pydantic import BaseModel, validator, EmailStr
+
+
+class PatientCreate(BaseModel):
+    id: str = None
+    # id: uuid.UUID = None
+    name: str
+    age: int
+    gender: str = "male"
+    phone: str
+    address: str
+    # doctor_id: uuid.UUID = None
+    doctor_id: str = None
+
+
+class PatientResponse(BaseModel):
+    # id: uuid.UUID
+    id: str
+    name: str
+    age: int
+    gender: str
+    phone: str
+    address: str
+    doctor_id: str
+    # doctor_id: uuid.UUID
+
+    @staticmethod
+    def parse(data):
+        """parses data"""
+        def parse_one(data):
+            return PatientResponse(**{
+                "id": data.id,
+                "name": data.name,
+                "age": data.age,
+                "gender": data.gender,
+                "phone": data.phone,
+                "address": data.address,
+                "doctor_id": data.doctor_id,
+            })
+        if isinstance(data, list):
+            return [parse_one(item) for item in data]
+        return parse_one(data)
+
+
+class PatientRecordCreate(BaseModel):
+    # id: uuid.UUID = None
+    id: str = None
+    date: datetime = datetime.now()
+    checkup_data: str = None
+    condition: str = None
+    # patient_id: uuid.UUID = None
+    patient_id: str = None
+
+
+class PatientRecordResponse(BaseModel):
+    # id: uuid.UUID = None
+    id: str = None
+    date: datetime = None
+    checkup_data: str = None
+    condition: str = None
+    # patient_id: uuid.UUID = None
+    patient_id: str = None
+
+    @staticmethod
+    def parse(data):
+        """parses data"""
+        def parse_one(data):
+            return PatientRecordResponse(**{
+                "id": data.id,
+                "date": data.date,
+                "checkup_data": data.checkup_data,
+                "condition": data.condition,
+                "patient_id": data.patient_id,
+            })
+        if isinstance(data, list):
+            return [parse_one(item) for item in data]
+        return parse_one(data)
