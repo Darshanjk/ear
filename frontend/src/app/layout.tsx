b4cb4fabd0b163d2cfcaa451d2cc22a6a898b2c5
Bhimraj Yadav <bhimrajyadav977@gmail.com>
2023-03-12 11:14:42 +0000

    Updated meta info

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,3 +1,23 @@
+export const metadata = {
+  title: "OtoScopeAI | A Healthcare App",
+  description: "Welcome to OtoScopeAI",
+  openGraph: {
+    title: "OtoScopeAI | A Healthcare App",
+    description: "Welcome to OtoScopeAI",
+    url: "https://otitis-media-detection.vercel.app",
+    siteName: "OtoScopeAI",
+    images: [
+      {
+        url: "https://otitis-media-detection.vercel.app/OtoScopeAI-A-Healthcare-App.png",
+        width: 1920,
+        height: 1080,
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
+};
+
 import "./globals.css";
 import { Poppins } from "next/font/google";
 import RecoilProvider from "./recoil";
@@ -6,6 +26,7 @@ const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
 });
+
 export default function RootLayout({
   children,
 }: {
