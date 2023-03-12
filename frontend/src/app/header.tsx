e87c79f33afa463df3041cf865c5be94a23aaecc
manisa-mdr <manishamanandhar0@gmail.com>
2023-03-12 11:15:51 +0545

    changes

--- frontend/src/app/header.tsx
+++ frontend/src/app/header.tsx
@@ -17,8 +17,7 @@ export default function Header({}: Props) {
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
           fill="currentColor"
-          className="h-6 w-6"
-        >
+          className="h-6 w-6">
           <path
             fillRule="evenodd"
             d="M2.25 4.125c0-1.036.84-1.875 1.875-1.875h5.25c1.036 0 1.875.84 1.875 1.875V17.25a4.5 4.5 0 11-9 0V4.125zm4.5 14.25a1.125 1.125 0 100-2.25 1.125 1.125 0 000 2.25z"
@@ -29,7 +28,7 @@ export default function Header({}: Props) {
       ),
       title: (
         <>
-          EarCare <span className="text-pink-600">IQ</span>
+          OtoScope<span className="text-black"> AI</span>
         </>
       ),
     },
@@ -48,10 +47,6 @@ export default function Header({}: Props) {
         title: "About Us",
         link: "/",
       },
-      {
-        title: "Health News",
-        link: "/",
-      },
     ],
     button: {
       title: user?.accessToken ? "Dashboard" : "Login",
@@ -59,25 +54,27 @@ export default function Header({}: Props) {
     },
   };
   return (
-    <header className="flex w-full py-6 border-b px-4">
-      <nav className="mx-auto flex w-full max-w-7xl flex-row items-center justify-between">
-        {/* Logo */}
-        <div className="flex cursor-pointer flex-row items-center space-x-2">
-          <span className="text-yellow-600">{content?.logo?.icon}</span>
-          <span className="text-2xl font-bold text-sky-800">
-            {content?.logo?.title}
-          </span>
-        </div>
+    <header className="grid grid-rows-2 w-full py-6 border-b px-4">
+      {/* Logo */}
+      <div className="flex cursor-pointer justify-center object-center flex-row  space-x-2">
+        <span className="text-yellow-600 items-center ">
+          {content?.logo?.icon}
+        </span>
+        <span className="text-4xl font-bold text-sky-800">
+          {content?.logo?.title}
+        </span>
+      </div>
+
+      <nav className="mx-auto flex w-full max-w-6xl flex-row items-center justify-between">
         {/* Menu Items */}
-        <ul className="hidden md:flex flex-row space-x-8">
+        <ul className="hidden md:flex flex-row text-xl space-x-8">
           {content?.menus &&
             content?.menus.map((item, i) => (
               <Link href={item?.link} key={i}>
                 <li
                   className={`${
-                    item?.active ? "font-medium text-sky-700" : "text-sky-600"
-                  } transition hover:text-sky-900`}
-                >
+                    item?.active ? "font-bold  text-sky-800" : "text-sky-600"
+                  } transition hover:text-black`}>
                   {item?.title}
                 </li>
               </Link>
--- frontend/src/app/heroSection.tsx
+++ frontend/src/app/heroSection.tsx
@@ -6,7 +6,7 @@ type Props = {};
 
 export default function HeroSection({}: Props) {
   const content = {
-    description: "Join us and be a life saver for each other.",
+    description: "Revolutionizing ear health with deep learning technology",
     cta_button: "Try Now",
     images: {
       a: "/doctor-using-laptop-thinking.jpg",
@@ -15,70 +15,39 @@ export default function HeroSection({}: Props) {
     },
   };
   return (
-    <section className="w-full">
-      <div className="mx-auto grid max-w-7xl grid-cols-1 pt-8 md:grid-cols-2">
-        {/* Main Content */}
-        <div className="flex flex-col space-y-3 py-12">
-          <h1 className="flex flex-col space-y-5 text-4xl sm:text-5xl lg:text-6xl font-bold text-sky-800">
-            <span> For Best </span>
-            <span>
-              <span className="border border-sky-500 px-4"> Medical</span> and{" "}
-              <span className="text-sky-500">Health </span>{" "}
-            </span>
-            <span>Care</span>
-          </h1>
-          <p className="text-lg text-sky-600">{content?.description} </p>
-
-          {/* Buttons */}
-          <div className="flex flex-row space-x-8 pt-10">
-            <Link href="try-now">
-              <button className="rounded-md bg-sky-900 px-10 py-3 font-semibold text-white transition hover:bg-sky-900/90 focus:outline-none">
-                {content?.cta_button}
-              </button>
-            </Link>
-            <button className="font-semibold text-sky-500 transition hover:text-sky-700 focus:outline-none">
-              See more
-            </button>
+    <section className="w-full  py-8">
+      {/* Background Image */}
+      <div
+        className="mx-auto max-w-5xl   justify-items-center object-fit "
+        style={{ backgroundImage: "url(/background.png)" }}>
+        <div className="mx-auto grid grid-cols-1 cursor-default pt-8 md:grid-cols-2 ">
+          {/* Main Content */}
+          <div className="flex flex-col p-6 space-y-3">
+            <h1 className="flex flex-col px-8 text-4xl sm:text-5xl lg:text-6xl font-bold text-black">
+              <span> For Best </span>
+              <span>
+                <span className=""> Medical</span> and{" "}
+                <span className="text-red-800  space-y-5 ">Health </span>{" "}
+              </span>
+              <span>Care</span>
+            </h1>
           </div>
         </div>
-        {/* Images */}
-        <div className="relative flex flex-col min-h-[38vh]">
-          <div className="absolute hidden sm:flex lg:left-16 top-1/3 z-10 -mt-2 h-48 w-96">
-            <Image
-              className=" object-contain "
-              src={content?.images?.a}
-              fill
-              priority
-              alt="Doctor a"
-            />
-          </div>
-          <div className="absolute hidden sm:flex right-4 top-4 z-10 h-[204px] w-[204px]">
-            <Image
-              className="object-cover"
-              src={content?.images?.b}
-              fill
-              priority
-              alt="Doctor b"
-            />
-          </div>
-          <div className="absolute right-4 bottom-4 z-10 h-[204px] w-[204px]">
-            <Image
-              className="object-cover"
-              src={content?.images?.c}
-              fill
-              priority
-              alt="Doctor c"
-            />
-          </div>
-          {/* patterns */}
-          <div className="absolute lg:left-60 bottom-0 z-10 flex flex-row">
-            <div className="flex flex-col items-end justify-end">
-              <div className="h-10 w-10 rounded-full bg-emerald-500"></div>
-              <div className="h-20 w-20 rounded-tl-full bg-yellow-400"></div>
-            </div>
-            <div className="h-20 w-20 rounded-br-full bg-sky-400"></div>
-          </div>
-          <div className="absolute right-0 z-0 h-full w-1/2 bg-sky-800"></div>
+
+        <div className="text-xl cursor-default  hover:scale-110 text-center text-black">
+          "{content?.description}"
+        </div>
+
+        {/* Buttons */}
+        <div className="flex flex-row justify-center space-x-8 p-6">
+          <Link href="try-now">
+            <button className="rounded-md bg-black px-10 py-3 font-semibold text-white transition hover:bg-sky-500 focus:outline-none">
+              {content?.cta_button}
+            </button>
+          </Link>
+          <button className="rounded-md bg-black px-10 py-3 font-semibold text-white transition hover:bg-sky-500 focus:outline-none">
+            See more
+          </button>
         </div>
       </div>
     </section>
